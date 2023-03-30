008771d03eab7f4be123ef8ac3e9589817c022e1
KaliAir <jhonjubenmallari@gmail.com>
2023-03-30 22:35:45 +0800

    added download file

--- src/components/DownloadCV.jsx
+++ src/components/DownloadCV.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {motion} from 'framer-motion';
 import {AiOutlineCloudDownload} from 'react-icons/ai'
+import Images from '../images/Images'
 
 const DownloadCV = ()=>{
 	return (
+		<a href={Images.cv} download className="download">	
 			<motion.button
 			whileHover={{
 				scale: 1.1,
@@ -11,6 +13,7 @@ const DownloadCV = ()=>{
 				boxShadow: "0 0 4px black"
 			}}
 			><AiOutlineCloudDownload style={{fontSize:"1.3rem"}}/> Download C.V</motion.button>
+		</a>	
 		)
 }
 
--- src/images/Images.jsx
+++ src/images/Images.jsx
@@ -2,7 +2,8 @@ const Images = {
 	pp: require('./pp.jpg'),
 	resume: require('./resume.jpg'),
 	night: require('./night.jpg'),
-	ss: require('./ss.jpg')
+	ss: require('./ss.jpg'),
+	cv: require('./cv.pdf')
 }
 
 export default Images;
